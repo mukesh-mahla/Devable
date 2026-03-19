c5cd0b91fba84d0fba2033324f9617c0f8df352a
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-03-19 22:55:30 +0530

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-const Page = ()=>{
+import { prisma } from "@/lib/db"
 
-  return <div>hello world</div>
+const Page =async ()=>{
+
+ const user = await prisma.user.findMany()
+  return <div>{JSON.stringify(user,null,2)}</div>
 }
 
 export default Page
