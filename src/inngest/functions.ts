65ed6b6ba1fc06bd3c2fea3dec20868be2e9dcf8
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-03-20 16:40:32 +0530

    e2b sandbox,and docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
+import {Sandbox} from "@e2b/code-interpreter"
 import { gemini, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world", triggers: [{ event: "test/hello.world" }] },
   async ({ event, step }) => {
+
+    const snadboxId = await step.run("get-sandbox-id",async()=>{
+      const sandbox = await Sandbox.create("nextjs-test-3")
+      return sandbox.sandboxId
+    })
 
     const codeAgent = createAgent({
       name: "code-agent",
@@ -14,6 +21,13 @@ export const helloWorld = inngest.createFunction(
     
        const { output } = await codeAgent.run(`summarize the following text:${event.data.value}`);
 console.log(output)
-    return { output };
+
+const sandboxUrl = await step.run("sandbox-url",async()=>{
+  const sandbox =  await getSandbox(snadboxId)
+ const host = sandbox.getHost(3000)
+  return `https:..${host}`
+   
+})
+    return { output,sandboxUrl };
   },
 );
